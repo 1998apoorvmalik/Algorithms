ff1cc3d172104422fa40b4115c6c2b618131c8db
1998apoorvmalik <34202100+1998apoorvmalik@users.noreply.github.com>
2018-12-24 03:19:16 -0800

    Update Graph_Representation_Python.py

--- Graph_Representation_Python.py
+++ Graph_Representation_Python.py
@@ -8,10 +8,3 @@ for _ in range(edges):
     x, y = map(int, input().split())
     adj_list[x].append(y)
     adj_list[y].append(x)
-
-for _ in range(int(input())):
-    a, b = map(int, input().split())
-    if b in adj_list[a]:
-        print("YES")
-    else:
-        print("NO")
