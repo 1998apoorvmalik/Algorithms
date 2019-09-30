fe155be6b0163b08c174c488395f52651a679591
1998apoorvmalik <apoorv258@gmail.com>
2019-09-30 15:45:41 +0530

    Added binary search algorithm in python

--- Searching/Binary Search/BinarySearch.py
+++ Searching/Binary Search/BinarySearch.py
@@ -6,65 +6,105 @@ Date:   30 September, 2019
 ==========================================
 """
 
+# Imports
 import random
 
 # Specify the parameters for the array.
 SIZE = 20
 LOW = -100
 HIGH = 100
+ELEMENT = 34
 
-def generateRandomArray(size, low, high):   # Utility function to generate random array of fixed size in range low -> high.
-    return [random.randint(low, high) for _ in range(size)]
-
-def mergeSort(array):
-    if len(array) > 1:                      # Ensure that the length of array is greater than one.
-        mid = len(array) // 2               # Finding the middle index of the array.
-        # Dividing the array into two halves.
-        L = array[:mid]                     # Left half of the array
-        R = array[mid:]                     # Right half of the array
-
-        mergeSort(L)                        # Sorting the first half of the array
-        mergeSort(R)                        # Sorting the second half of the array
-
-        i = j = k = 0
-
-        while i < len(L) and j < len(R):    # Combining the L and R arrays and copying the data from them.
-            if L[i] < R[j]:
-                array[k] = L[i]
-                i += 1
-            else:
-                array[k] = R[j]
-                j += 1
-            k += 1
-        
-        while i < len(L):                   # Ensuring that no element is leftto copy from the L array.
-            array[k] = L[i]
-            i += 1
-            k += 1
-        
-        while j < len(R):                   # Ensuring that no element is left to copy from the R array.
-            array[k] = R[j]
-            j += 1
-            k += 1
-    
-    return array
+# Utility function to generate random sorted array of fixed size in range low -> high. Also, include element 
+# in the array if element argument is not None.
+def generateRandomSortedArray(size, low, high, element=None): 
+    temp = [random.randint(low, high) for _ in range(size)]
+    if element != None:
+        temp.append(element)
+    temp = list(set(temp))
+    temp.sort()
+    return temp
 
 def printArray(array):                      # Utility function to print an array.
     for i in array:
         print(i, end = ' ')
+    print("\n")
 
-# Generating a random array.
-array = generateRandomArray(SIZE, LOW, HIGH)
+# Python Program for recursive binary search. Function will return the index of the element in the array, if present, 
+# else it will return -1.
 
-# Printing the unsorted array.
-print("\nUnsorted array is :")
-printArray(array)
-print("\n\n")
+def binarySearchRecursive(array, element, l, r):
+    # Check base case,
+    if r >= l:
+        
+        # Define the middle index of the array.
+        mid = l + (r - l)/2
+        mid = int(mid)  # Converting to int.
 
-# Sorting the random array using merge sort.
-array = mergeSort(array)
+        # If element is present at the middle itself, then return mid index.
+        if array[mid] == element:
+            return mid
+        
+        elif array[mid] > element:  # The element is not present in the right subarray, search in the left subarray.
+            return binarySearchRecursive(array, element, l, mid-1,)
+        
+        else:   # The element is not present in the left subarray, search in the right subarray.
+            return binarySearchRecursive(array, element, mid+1, r)
+    
+    else:
+        # Element is not present in the array.
+        return -1
 
-# Printing the sorted array.
-print("Sorted array is :")
-printArray(array)
-print("\n\n")
+# Python Program for iterative binary search. Function will return the index of the element in the array, if present, 
+# else it will return -1.
+def binarySearchIterative(array, element):
+    l = 0
+    r = len(array) - 1
+
+    while l <= r:
+
+        # Define the middle index of the array.
+        mid = l + (r - l)/2
+        mid = int(mid)  # Converting to int.
+
+        # If element is present at the middle itself, then return mid index.
+        if array[mid] == element:
+            return mid
+        
+        elif array[mid] > element:  # The element is not present in the right subarray, search in the left subarray.
+            r = mid - 1
+        
+        else:   # The element is not present in the left subarray, search in the right subarray.
+            l = mid + 1
+    
+    # Element is not present in the array.
+    return -1
+
+# Driver code to test the above code.
+if __name__ == '__main__': 
+    # Generating a random sorted array.
+    array = generateRandomSortedArray(SIZE, LOW, HIGH, ELEMENT)
+
+    # Printing the sorted array.
+    print("Sorted array is :")
+    printArray(array)
+
+    # Finding the index of the element in the array using recursive binary search.
+    print("Searching element '{}' using Recursive Binary Search:".format(ELEMENT))
+    index = binarySearchRecursive(array, ELEMENT, 0, len(array)-1)
+    if index != -1:
+        print("The element is found at index: ", index)
+    else:
+        print("The element is not present in the array")
+    
+    print("\n")     # Extra space
+
+    # Finding the index of the element in the array using iterative binary search.
+    print("Searching element '{}' using Iterative Binary Search:".format(ELEMENT))
+    index = binarySearchIterative(array, ELEMENT)
+    if index != -1:
+        print("The element is found at index: ", index)
+    else:
+        print("The element is not present in the array")
+    
+    print("\n")     # Extra space
